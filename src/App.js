efe93ab1fa95158fcc700e515215478ced70f7e3
chochiniz <chochinize@gmail.com>
2021-07-19 16:59:48 +0200

    check the icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavBar from './Components/NavBar/index';
-
+import {AiFillAccountBook} from 'react-icons/ai'
 function App() {
   return (
     <div className="">
       <Router>
+        <AiFillAccountBook />
         <NavBar />
         <h1>YOUTUBdasdasE</h1>
       
