ec739942546d5f2797309a9b9fbb7cc070cc3513
chochiniz <chochinize@gmail.com>
2021-07-19 16:38:53 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NavBar from './Components/NavBar/index';
 function App() {
   return (
     <div className="">
       <Router>
+        <NavBar />
         <h1>YOUTUBdasdasE</h1>
+      
       </Router>
     </div>
   );
