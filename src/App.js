9ed611e69d7980f18bcc0c6b97a31a3a4dc36cf3
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-07-19 12:58:00 +0200

    imported Components from react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 function App() {
   return (
     <div className="">
-
+      <Router>
+        
+      </Router>
     </div>
   );
 }
