fd27955838d7d4d843f7d5c0825dfd3750ab1f64
chochiniz <chochinize@gmail.com>
2021-07-19 16:17:38 +0200

    adding some functionality

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const index = () => {
     return (
         <div>
+            <h1>ADDING SOME FUNCTIONALITY</h1>
             <h1>HELLO BARNAV</h1>
             <h2>I WANdasdasdNA MAP</h2>
         </div>
