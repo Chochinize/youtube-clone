625a738e21b05d01f4ca5f0b1fc829c320d4cba7
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-07-20 12:14:08 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavigationBar from './Components/NavBar/index';
-import {AiFillAccountBook} from 'react-icons/ai'
-import TopLogo from './Components/SideBar/index';
 
 
 function App() {
-  console.log(Router)
   return (
     <div className="">
       <Router>
-        <AiFillAccountBook />
         <NavigationBar />
-        <h1></h1>
-
-        <TopLogo />
-      
       </Router>
     </div>
   );
