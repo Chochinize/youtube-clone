332d34d694d80dfb041e9a10d31a3e20be3420df
Lucas Ferrari <73648865+Lnferrari@users.noreply.github.com>
2021-07-20 12:12:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavigationBar from './Components/NavBar/index';
-import {AiFillAccountBook} from 'react-icons/ai'
-import TopLogo from './Components/SideBar/index';
 
 
 function App() {
