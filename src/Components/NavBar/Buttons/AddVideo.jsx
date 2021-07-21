e9b31f966ec856c2f9cefcb747bf9295d98c8b19
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-07-21 12:54:32 +0200

    added icons into navbar

---
+++ src/Components/NavBar/Buttons/AddVideo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {MdVideoCall as VideoIcon} from 'react-icons/md'
+
+function AddVideo() {
+    return (
+        <>
+            <VideoIcon size={25}/>
+        </>
+    )
+}
+
+export default AddVideo
---
+++ src/Components/NavBar/Buttons/Notifications.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {MdNotifications as Notification} from 'react-icons/md'
+
+function Notifications() {
+    return (
+        <>
+            <Notification size={25} />
+        </>
+    )
+}
+
+export default Notifications
