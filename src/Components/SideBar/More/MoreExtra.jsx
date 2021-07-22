af880f8913b245f03e41913dca2a9c196ab95c34
chochiniz <chochinize@gmail.com>
2021-07-22 10:41:34 +0200

    added everywhere Link

--- src/Components/SideBar/More/MoreExtra.jsx
+++ src/Components/SideBar/More/MoreExtra.jsx
@@ -3,32 +3,44 @@ import {RiSettings3Fill as Settings} from 'react-icons/ri'
 import {MdFlag as Flag} from 'react-icons/md'
 import {MdHelp as Help} from 'react-icons/md'
 import {RiFeedbackFill as Feedback} from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 
 const MoreExtra = () => {
     return (
         <div className='sideBar-main'>
         <section className='side-section'>
         
-    <h4 className='position'>
+
+        <Link to='/Settings'>
+    <h4 className='position' data-tip='Settings'>
        <Settings className='icons' size={20}/>
-        <a href='#' className='text'>Settings</a>
+        <span href='#' className='text'>Settings</span>
        </h4>
+        </Link>
       
-       <h4 className='position'>
+
+      <Link to='/Report History'>
+       <h4 className='position' data-tip='Report History'>
        <Flag className='icons' size={20}/>
         <span className='text'>Report History</span>
        </h4>
+      </Link>
 
-       
-       <h4 className='position'>
+
+       <Link to='/Help'>
+       <h4 className='position' data-tip='Help'>
        <Help className='icons' size={20}/>
         <span className='text'>Help</span>
        </h4> 
+       </Link>
         
-       <h4 className='position'>
+
+        <Link to='/Send feedback'>
+       <h4 className='position' data-tip='Send feedback'>
        <Feedback className='icons' size={20}/>
         <span className='text'>Send feedback</span>
        </h4>
+        </Link>
 
 
         </section>
