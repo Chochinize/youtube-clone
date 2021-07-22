fe0cd691ef874bddcd07a4dc97263953e8c4dd70
chochiniz <chochinize@gmail.com>
2021-07-22 17:01:34 +0200

    adding side bar to the page

---
+++ src/Components/smallBar/smallSubscription/SmallLibrary.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {MdVideoLibrary as Library }from 'react-icons/md';
+const SmallSubscriptions = () => {
+    return (
+        <div>
+            
+        <div className='main'>
+            <Library size={20} className='icons'/>
+            <h4 className='text'>Library</h4>
+        </div>
+
+    </div>
+    )
+}
+
+export default SmallSubscriptions
