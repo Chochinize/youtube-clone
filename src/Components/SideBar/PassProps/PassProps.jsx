3bf500cea10ff05a7a4cc9becb3e6ee3e3496e7e
chochiniz <chochinize@gmail.com>
2021-07-27 11:31:20 +0200

    latest push

--- src/Components/SideBar/PassProps/PassProps.jsx
+++ src/Components/SideBar/PassProps/PassProps.jsx
@@ -1,33 +1,12 @@
 import React from 'react'
-import ReactPlayer from 'react-player';
-import { Link , useParams} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const PassProps = ({properties}) => {
-    
-    
-    const param = useParams();
 
 
-
-
-    
-    
- 
+  
     return (
         <div>
-            <tempalte>
-                    
-     
-{/*             
-            <ReactPlayer
-             className='react-player'
-             url={`https://www.youtube.com/watch?v=${param}`}
-             controls={true} 
-             width='100%' 
-             height='100%' 
-             /> */}
-        
-            
-            </tempalte>
+            {properties}
         </div>
     )
 }
